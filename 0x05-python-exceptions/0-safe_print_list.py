e98dae1af3088857e38d6adcc1ad243d868e8a91
Imeogu <stephenimeogu@gmail.com>
2022-06-20 17:11:10 -0700

    Imeogu

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
+"""
+    prints x elements of a list.
 
-    for i in range(x):
+    """
+
+
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    for x in range(0, x):
         try:
-            print("{}".format(my_list[i]), end='')
+            print(my_list[x], end="")
+            nb += 1
         except:
             break
-        else:
-            count += 1
-
-    print()
-    return (count)
+    print("")
+    return(nb)
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+    prints an integer with "{:d}".format().
+
+    """
+
+
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        print('{:d}'.format(value))
+        return(True)
     except:
-        return (False)
-    else:
-        return (True)
+        return False
