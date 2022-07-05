ea4e41c667f57309ffa0fd3b4d265ec3d497b5bb
Imeogu <stephenimeogu@gmail.com>
2022-07-05 13:32:25 -0700

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-''' function that returns an object (Python data structure) represented by a JSON string
-'''
-
+""" 4-from_json_string: def from_json_string """
 import json
 
 
 def from_json_string(my_str):
-    ''' module from_json_string
-     returns Python objects
-    '''
+    """
+        returns an object represented by a JSON string
+        Args:
+            my_str: string to be decoded
+    """
     return json.loads(my_str)
